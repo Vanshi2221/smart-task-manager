edd7438e12eafc20ee6cb0b354eba35d25c3cbf4
Vanshika Gupta <vgvanshi2221@gmail.com>
2026-02-12 20:12:18 +0000

    refactor(server): setup MVC structure

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// test env loading
+console.log("ENV CHECK ->", process.env.MONGOURI);
+
 
 app.get('/', (req, res)=>{
     res.send("API running");
 })
 
-app.listen(5000, ()=>{
-    console.log("Server running on 5000");
-})
+app.listen(process.env.PORT, ()=>{
+    console.log("Server running on ",process.env.PORT)
+});
